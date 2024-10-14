19cdaf644c29f19cd4a9310eba2ac69af72b8f01
ovitudev <maximianodev@gmail.com>
2024-10-14 18:58:52 -0300

    Correção do loop

--- Program.cs
+++ Program.cs
@@ -3,16 +3,16 @@ class Program
 {
     static void Main()
     {
-        Console.Clear();
+
+
         // Inicializando chave de leitura
         ConsoleKeyInfo keyinfo;
-
-
-        Controller.TelaPrincipal();
-        keyinfo = Console.ReadKey(true); // Melhor interação para o usuário
-
         do
         {
+            Console.Clear();
+            Controller.TelaPrincipal();
+            keyinfo = Console.ReadKey(true); // Melhor interação para o usuário
+
             if (keyinfo.Key == ConsoleKey.NumPad1 || keyinfo.Key == ConsoleKey.D1)
             {
                 Console.Clear();
@@ -21,9 +21,9 @@ class Program
                 {
                     Console.WriteLine("Pressione qualquer tecla para voltar ao menu principal...");
                     Console.ReadKey(true);
-                    
+
                 }
-                    
+
             }
 
             if (keyinfo.Key == ConsoleKey.NumPad2 || keyinfo.Key == ConsoleKey.D2)
@@ -32,7 +32,7 @@ class Program
                 Controller.ObjetosEncontrados();
                 Console.ReadKey(true);
             }
-
+            continue;
         } while (keyinfo.Key != ConsoleKey.NumPad0 || keyinfo.Key != ConsoleKey.D0);
     }
 }
